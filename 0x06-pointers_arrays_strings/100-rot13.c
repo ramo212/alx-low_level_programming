28359c6fbf72cb36bbcc93d1160e33feb0e4c281
ramo212 <zanbouaahmad@gmail.com>
2023-09-20 16:29:46 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - function that encodes a string
+ * @i: input
+ * Return: i
+ */
+char *rot13(char *i)
+{
+	int x, y;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char data2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++;)
+	{
+		for (y = 0; y < 52; y++)
+		{
+			if (s[x] == data1[y])
+			{
+				s[x] = data2[y];
+				break;
+			}
+		}
+	}
+	return (i);
+}
