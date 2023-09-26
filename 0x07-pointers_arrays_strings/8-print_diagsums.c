02f8ceac487db53cf95a0881e2a4f26b1e5a6b63
ramo212 <zanbouaahmad@gmail.com>
2023-09-26 13:10:21 -0700

    EVEN More

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * set_string - function
+ * @s: input
+ * @to: input
+ * Return: 0
+ */
+void set_string(char **s, char *to)
+{
+	int sum1 = 0;
+	int sum2 = 0;
+	int x;
+
+	for (x = 0; x < size; x++)
+	{
+		sum1 = sum1 + [x * size + x];
+	}
+	for (x = size - 1; x >= 0; x--)
+	{
+		sum2 += a[x * size + (size - x - 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
